153d39b5f13baf7f26a2849e1cccfd9fed686310
Akshat Kumar Jha <jhakshat24@gmail.com>
2025-10-21 10:46:38 +0530

    Added codes for forge create commands

--- internal/forge/create.go
+++ internal/forge/create.go
@@ -2,9 +2,31 @@ package forge
 
 import (
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
-func Create() error {
-	fmt.Println("Forge create executed Successfully")
+func Create(contractName string, flags ...string) error {
+	if contractName == "" {
+		return fmt.Errorf("Contract Name has to be given")
+	}
+
+	args := []string{"create", contractName}
+	args = append(args, flags...)
+
+	cmd := exec.Command("forge", args...)
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return fmt.Errorf("forge create failed: %w\noutput: %s", err, string(output))
+	}
+
+	fmt.Println("Forge create executed Successfully!")
+
+	if len(output) > 0 {
+		fmt.Println(strings.TrimSpace(string(output)))
+	}
+
 	return nil
 }
