77ebe07e51b6918b585fcec3c68776c9279f3974
Akshat Kumar Jha <jhakshat24@gmail.com>
2025-10-21 10:03:15 +0530

    Added working codes for forge init commands

--- internal/forge/init.go
+++ internal/forge/init.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 )
 
-func Init() error {
+func Init(flags ...string) error {
+	args := {"init"}
+	args = append(args, flags...)
+
+	cmd := exec.Command("forge", args...)
+
+	output, err = cmd.CombinedOutput()
+
+	if err != nil {
+		return fmt.Errorf("forge test failed: %w\nOutput: %s", err, string(output))
+	}
+
 	fmt.Println("Forge init executed Successfully!")
+
+	if len(output) > 0 {
+		fmt.Println(strings.TrimSpace(string(output)))
+	}
+
 	return nil
 }
